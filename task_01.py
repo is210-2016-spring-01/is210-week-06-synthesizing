e4fa93cf86101415e470be31021233a8a4eeba0a
ct3080a <coleen.thomas@spsmail.cuny.edu>
2016-03-06 05:12:45 +0000

    week six synthesizing

---
+++ task_01.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Table seating lists"""
+
+def get_party_stats(families=([]), table_size=6):
+    """Sorts and places families for party.
+
+    Args:
+        families (list): list of names
+        table_size (int, optional): amount of people at table. Default: 6
+
+    Returns:
+        tuple: total number of guests and total number of tables 
+
+    Examples:
+        >>> get_party_stats(['Jan'], ['Jen', 'Jess'], ['Jem', 'Jack', 'Janis']])
+        (6, 3)
+
+    """
+    party_index = 0
+    for party_item in get_party_stats:
+        party_index
+    -(-len([families])//table_size)
+
+    return (len([families]), table_size)
