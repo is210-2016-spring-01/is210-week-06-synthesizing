0d4ffa4b8b45c3c041fc5619402eea7e24159a68
Daniella Sapozhnikova <daniellasapo@gmail.com>
2016-03-07 09:34:39 +0000

    still sad face

--- task_02.py
+++ task_02.py
@@ -7,13 +7,12 @@ def prepare_email(*appointments):
     """appointments, A list of two-item tuples with the client's name
         and their appointment time as members"""    
     email ='Dear {},\nI look forward to meeting with you on {}.\nBest,\nMe'
-
-    Name1 = body[0][0]
-    Time1 = body[0][1]
-
-    Name2 = body[1][0]
-    Time2 = body[1][1]
     for body in appointments:
+        Name1 = body[0][0]
+        Time1 = body[0][1]
+
+        Name2 = body[1][0]
+        Time2 = body[1][1]
     
         body1 = email.format(Name1, Time1)
         body2 = email.format(Name2, Time2)
