4f09316a508b828e24ff699f7b156b6a2d46a8f9
paisely65 <cunysps@gmail.com>
2016-03-06 22:46:35 -0500

    Task 1 is scompleted

---
+++ task_01.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This docstring creates seating charts."""
+
+def get_party_stats(families, table_size):
+    """This function organizes seating.
+
+    Arg:
+        families (str): A list of names.
+        table_size (int): The maximum number of seats at each table.
+
+    Returns:
+        mxed: Families and the tables they sit at.
+
+    Examples:
+        
+    """
+    for i in 'families:
+        print total
+
+    for i in table_size:
+        print total
+    
+
+ 
+
+    
