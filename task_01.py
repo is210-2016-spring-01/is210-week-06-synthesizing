81457a5c065b75da1366e60f8ef4e2c8ef0adce0
ct3080a <coleen.thomas@spsmail.cuny.edu>
2016-03-07 03:31:40 +0000

    final attempt

--- task_01.py
+++ task_01.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 """Table seating lists"""
 
-def get_party_stats(families=([]), table_size=6):
+
+def get_party_stats(families, table_size=6):
     """Sorts and places families for party.
 
     Args:
@@ -10,16 +11,12 @@ def get_party_stats(families=([]), table_size=6):
         table_size (int, optional): amount of people at table. Default: 6
 
     Returns:
-        tuple: total number of guests and total number of tables 
-
+        tuple: total number of guests and total number of tables
+        
     Examples:
         >>> get_party_stats(['Jan'], ['Jen', 'Jess'], ['Jem', 'Jack', 'Janis']])
         (6, 3)
 
     """
-    party_index = 0
-    for party_item in get_party_stats:
-        party_index
-    -(-len([families])//table_size)
 
-    return (len([families]), table_size)
+    return (len([families]), -(-len([families])//table_size))
--- task_02.py
+++ task_02.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 """Appointment Tracker"""
 
+
 def prepare_email(appointments):
     """lists appointments and client names.
 
@@ -10,11 +11,11 @@ def prepare_email(appointments):
 
     Returns:
         list: full client list with appointments
-        
+
     Examples:
         >>> prepare_email([('Jen', '2015'), ('Max', 'March 3')]
         ['Dear Jen,\nI look forward to meeting with you on 2015.\nBest,\nMe',
         'Dear Max,\nI look forward to meeting you on March 3.\nBest\nMe']
     """
 
-    return '{}, {}, {},'.format(appointments)
+    return 'Dear {}, \nI look forward to meeting with you on {}.\nBest, \nMe'.format(appointments)
